2f71f1220421411ad4c18250d797d426c859963c
Givi Chopikashvili <jager@djem.ru>
2023-12-07 23:22:15 +0300

    7b django chained

--- 7b.js
+++ 7b.js
@@ -6,11 +6,10 @@ QQQJA 483`;
 
 const cardStrength = ['J', '1','2','3','4','5','6','7','8','9','T','Q','K','A'];
 const adjustHand = hand => hand.split('').map(card => String.fromCharCode(65 + cardStrength.indexOf(card))).join('');
-const getStrength = (hand) => {
-    const data = Object.values(hand.split('').filter(card => card !== 'J').reduce((acc, card) => (acc[card] = (acc[card] || 0) + 1, acc), {})).sort((a, b) => b - a);
-    data[0] = (data[0] || 0) + hand.split('').filter(card => card === 'J').length;
-    return data.join('')
-}
+const getStrength = (hand) => Object.values(hand.split('').filter(card => card !== 'J').reduce((acc, card) => (acc[card] = (acc[card] || 0) + 1, acc), {}))
+    .sort((a, b) => b - a)
+    .concat(0).map((v, i) => i ? v : v + hand.split('').filter(card => card === 'J').length)
+    .join('');
 
 const betSum = [...values.matchAll(/(.{5}) (\d+)/g)]
     .map(m => ({strength: getStrength(m[1]), adjusted: adjustHand(m[1]), amount: m[2]}))
