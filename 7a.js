85950eb8b9ceef09795c0e51204711fb307bdd12
Givi Chopikashvili <jager@djem.ru>
2023-12-07 22:36:59 +0300

    7a

---
+++ 7a.js
@@ -0,0 +1,16 @@
+const values = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`;
+
+const cardStrength = ['1','2','3','4','5','6','7','8','9','T','J','Q','K','A'];
+const adjustHand = hand => hand.split('').map(card => String.fromCharCode(65 + cardStrength.indexOf(card))).join('');
+const getStrength = (hand) => Object.values(hand.split('').reduce((acc, card) => (acc[card] = (acc[card] || 0) + 1, acc), {})).sort((a, b) => b - a).join();
+
+const betSum = [...values.matchAll(/(.{5}) (\d+)/g)]
+    .map(m => ({strength: getStrength(m[1]), adjusted: adjustHand(m[1]), amount: m[2]}))
+    .sort((a, b) => a.strength.localeCompare(b.strength) || a.adjusted.localeCompare(b.adjusted))
+    .reduce((sum, hand, i) => sum + hand.amount * (i + 1), 0);
+
+console.log(betSum);
