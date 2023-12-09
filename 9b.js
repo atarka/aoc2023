cfc3dd2512ba2d48cfa1fe26005a331860c1db2d
Givi Chopikashvili <jager@djem.ru>
2023-12-09 10:42:35 +0300

    8b

---
+++ 9b.js
@@ -0,0 +1,12 @@
+const values = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+const calibara = (range) => {
+    const diffRange = range.map((v, i, a) => i ? v - a[i - 1] : 0).slice(1);
+    if (diffRange.every(v => !v)) return range.at(0) || 0;
+    return range.at(0) - calibara(diffRange);
+};
+
+const result = values.split(/\n/).map(row => row.split(' ').map(Number)).map(calibara).reduce((sum, v) => sum + v, 0);
+console.log(result);
