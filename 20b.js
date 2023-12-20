f029f24ce5d3f36a06e11a27623fa213c38c03b1
Givi Chopikashvili <jager@djem.ru>
2023-12-20 17:15:10 +0300

    20b removed unnecessary

--- 20b.js
+++ 20b.js
@@ -5,33 +5,29 @@ const values = `broadcaster -> a, b, c
 &inv -> a`;
 
 const devices = {};
-let totalFlops = 0;
-let totalFlopsOn = 0;
 
 [...values.matchAll(/([%&])?(.+?) -> (.+)/g)].forEach(([, type, name, connections]) => {
     if (!devices[name]) devices[name] = {from: [], to: [], state: []};
     devices[name].type = type;
-    if (type === '%') ++totalFlops;
 
     connections.split(', ').forEach(connectedTo => {
         devices[name].to.push(connectedTo);
         if (!devices[connectedTo]) devices[connectedTo] = {from: [], to: [], state: [], type: '-'};
         devices[connectedTo].from.push(name);
+        devices[connectedTo].state.push(false);
     })
 })
 
 
 const getButtonPresses = () => {
-    let pulses = [];
-    let reps = {};
+    const reps = {};
 
-    outer: for (let button = 1; ; ++button) {
-        pulses = [{ to: 'broadcaster', from: 'button', type: false }];
+    outer: for (let button = 1; devices.rx; ++button) {
+        const pulses = [{ to: 'broadcaster', from: 'button', type: false }];
 
-        for (let i = pulses.length - 1; i < pulses.length; ++i) {
+        for (let i = 0; i < pulses.length; ++i) {
             const { from, to, type } = pulses[i];
-            if (type && !reps[from] &&
-                devices[devices.rx.from[0]].from.includes(from)) {
+            if (type && devices.rx && !reps[from] && devices[devices.rx.from[0]].from.includes(from)) {
                 reps[from] = button;
                 if (Object.keys(reps).length === devices[devices.rx.from[0]].from.length) {
                     return Object.values(reps).reduce((acc, m) => acc * m, 1);
@@ -40,27 +36,17 @@ const getButtonPresses = () => {
 
             const device = devices[to];
 
-            switch (device.type) {
-                case '%':
-                    if (!type) {
-                        device.state[0] = !device.state[0];
-                        totalFlopsOn += device.state[0] ? 1 : -1;
-                        device.to.forEach(sendTo => pulses.push({ to: sendTo, from: to, type: device.state[0] }));
-                    }
-                    break;
-                case '&':
-                    let sendType = false;
-                    device.state[device.from.indexOf(from)] = type;
-                    for (let j = 0; j < device.from.length; ++j) {
-                        if (!device.state[j]) sendType = true;
-                    }
-                    device.to.forEach(sendTo => pulses.push({ to: sendTo, from: to, type: sendType, i }));
-                    break;
-                default:
-                    device.to.forEach(
-                        sendTo => pulses.push(
-                            { to: sendTo, from: to, type: false }));
-                    break;
+            if (device.type === '%') {
+                if (!type) {
+                    device.state[0] = !device.state[0];
+                    device.to.forEach(sendTo => pulses.push({ to: sendTo, from: to, type: device.state[0] }));
+                }
+            } else if (device.type === '&') {
+                device.state[device.from.indexOf(from)] = type;
+                const sendType = !device.state.slice(0, device.from.length).every(Boolean);
+                device.to.forEach(sendTo => pulses.push({ to: sendTo, from: to, type: sendType }));
+            } else {
+                device.to.forEach(sendTo => pulses.push({ to: sendTo, from: to, type: false }));
             }
         }
     }
